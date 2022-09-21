464c11c20974e043034bcb86f86bcdcd690ba88f
Kekuz <anton.edlenko@yandex.ru>
2022-09-21 13:03:22 +0300

    task3 rgb to hsv

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from PIL import Image
 from time import sleep
 from matplotlib import pyplot as plt
@@ -56,10 +57,34 @@ def task2():
     #blue_image.show()
     #plt.stairs(red_image.histogram())
     #plt.show()
+def rgb_to_hsv(r, g, b):
+    r, g, b = r/255.0, g/255.0, b/255.0
+    mx = max(r, g, b)
+    mn = min(r, g, b)
+    df = mx-mn
+    if mx == mn:
+        h = 0
+    elif mx == r:
+        h = (60 * ((g-b)/df) + 360) % 360
+    elif mx == g:
+        h = (60 * ((b-r)/df) + 120) % 360
+    elif mx == b:
+        h = (60 * ((r-g)/df) + 240) % 360
+    if mx == 0:
+        s = 0
+    else:
+        s = (df/mx)*100
+    v = mx*100
+    #return round(h), round(s), round(v)
+    return h, s, v
+
+
+
+
 
 
 if __name__ == '__main__':
-    task1()
-    task2()
-
+    #task1()
+    #task2()
+    pass
 
